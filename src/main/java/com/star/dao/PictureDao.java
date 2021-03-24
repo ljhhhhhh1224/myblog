b994bb3164c876b0a06c4db12d0644811cffce89
ljhhhhhh1224 <3364879467@qq.com>
2021-03-24 21:19:43 +0800

    Initial commit

---
+++ src/main/java/com/star/dao/PictureDao.java
@@ -0,0 +1,24 @@
+package com.star.dao;
+
+import com.star.entity.Picture;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Mapper
+@Repository
+public interface PictureDao {
+
+    List<Picture> listPicture();
+
+    int savePicture(Picture picture);
+
+    Picture getPicture(Long id);
+
+    int updatePicture(Picture picture);
+
+    void deletePicture(Long id);
+
+}
